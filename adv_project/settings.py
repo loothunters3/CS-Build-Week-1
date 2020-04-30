8a0fac3bbe7a5bef17f5cef7f434dfab22d68012
DNason1999 <50815684+DNason1999@users.noreply.github.com>
2020-04-30 15:50:33 -0500

    'updates'

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -146,10 +146,9 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_ALLOW_ALL=True
 
 CORS_ORIGIN_WHITELIST = [
-    "https://example.com",
-    "https://sub.example.com",
     "http://localhost:8080",
-    "http://127.0.0.1:9000"
+    "http://127.0.0.1:9000",
+    "http://localhost:3000"
     ]
 
 # Internationalization
