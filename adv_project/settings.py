b850240bf18e2fbce7618aa354023b452fdeb801
DNason1999 <50815684+DNason1999@users.noreply.github.com>
2020-04-30 10:49:00 -0500

    hello2

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', '.herokuapp.com', '.localhost', 'netlify.com', 'localhost', *]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
